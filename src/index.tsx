68848b3f43bc23ea043ecca264449fa96b4f5178
Atef Ben Ali <atef.bettaib@gmail.com>
2022-03-17 07:48:18 +0100

    chore: clean-up

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,8 @@ import ReactDOM from "react-dom";
 
 import { Main } from "./components/Main";
 import configureStore from "./store/store";
-// import * as designActions from './actions/design_actions';
-// import { signup, login, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
-  // const store = configureStore();
-
   let store;
   // @ts-expect-error ts-migrate(2339) FIXME: Property 'currentUser' does not exist on type 'Win... Remove this comment to see the full error message
   if (window.currentUser) {
@@ -27,18 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
-  // window.signup = signup;
-  // window.login = login;
-  // window.logout = logout;
-  // window.requestOwnedDesigns = designActions.requestOwnedDesigns;
-  // window.requestTemplates = designActions.requestTemplates;
-  // window.requestDesign = designActions.requestDesign;
-  // window.createDesign = designActions.createDesign;
-  // window.updateDesign = designActions.updateDesign;
-  // window.deleteDesign = designActions.deleteDesign;
 
   const root = document.getElementById("root");
   ReactDOM.render(<Main store={store} />, root);
